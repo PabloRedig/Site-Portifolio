39e09277108073ccf1c8f3fa2ce32b0e15635e27
Pablo Redig <pabloleaoredig@hotmail.com>
2023-01-25 14:09:07 -0300

    Estrutura do Projeto concluido

--- Scripts/Scripts.js
+++
@@ -1,7 +0,0 @@
-var navHide = document.getElementById("Nav-Hide");
-var menuicon = document.getElementById("menu-icon");
-
-menuicon.onclick = function () {
-    navHide.classList.toggle("open");
-
-};
